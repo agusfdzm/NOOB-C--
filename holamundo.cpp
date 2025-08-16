777daf77d2b2271f45f44bf4ba58c20a4fc4da2d
agusfdzm <129664400+agusfdzm@users.noreply.github.com>
2025-08-16 14:48:52 +0200

    Variables

--- holamundo.cpp
+++ holamundo.cpp
@@ -4,3 +4,12 @@ int main() {
     std::cout<<"Hola mundo";
     return 0;
 }
+
+// Comentario de una línea
+
+/*
+Comentario
+de
+varias
+líneas
+*/
---
+++ variables.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+
+    // Numeros enteros
+    int n1 = 5;
+    int n2 = 1;
+    int suma = n1 + n2;
+    std::cout<<suma;
+
+
+    // Float o dobles
+    double examen1 = 7.2;
+    double examen2 = 6.5;
+    double examen3 = 8.9;
+
+    // Char. Solo almacena 1 caracter
+    char calificacion = 'A';
+    char inicial = 'N';
+
+    // Booleano
+    bool estudiante = true;
+    bool estudiante2 = false;
+
+    // Strings
+    std::string nombre = "Agus";
+}
